1b88f1a9b692a40f74779acc5304ddd132e3e47e
elenaskovo <elenaskovorodko@Elenas-Air.lan>
2023-07-07 11:08:20 -0400

    newTestProject

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,15 @@
+package pages;
+
+public class LoginPage {
+    public static void sum (int num1,int num2){
+        //int sum=0;
+        System.out.println(num1+num2);
+    }
+
+    public static void main(String[] args) {
+        sum(20,10);
+        sum(40,60);
+        sum(200,200);
+
+    }
+}
